02f0221f76b34038354ac5d65f093a2d66ccd4c7
six <francis.i.authority@gmail.com>
2025-12-31 13:12:27 +0100

    ...

--- script.js
+++ script.js
@@ -4,9 +4,28 @@ document.getElementById("year").textContent = new Date().getFullYear();
 // Copy email functionality
 const copyBtn = document.getElementById("copyBtn");
 const emailText = document.getElementById("email").textContent;
-
+/* 
 copyBtn.addEventListener("click", () => {
   navigator.clipboard.writeText(emailText);
+  copyBtn.textContent = "Copied!";
+  setTimeout(() => {
+    copyBtn.textContent = "Copy Email";
+  }, 1500);
+});
+ */
+
+copyBtn.addEventListener("click", () => {
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(emailText);
+  } else {
+    const textarea = document.createElement("textarea");
+    textarea.value = emailText;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+  }
+
   copyBtn.textContent = "Copied!";
   setTimeout(() => {
     copyBtn.textContent = "Copy Email";
